917fe93fded1725d63213ba80a674a1916a297a1
Alejandro Saucedo <axsauze@gmail.com>
2021-12-18 15:34:28 +0000

    Added check for owner

--- contracts/deposit.sol
+++ contracts/deposit.sol
@@ -1,20 +1,33 @@
+//SPDX-License-Identifier: BSD-2.0
 
-pragma solidity 0.8.0;
+pragma solidity 0.8.10;
 
 contract Deposit {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     receive() external payable{
     }
+
     fallback() external payable{
     }
+
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
+
     function sendEther() public payable {
         uint x;
         x++;
     }
 
-    function transferEther(address payable receipient, uint amount) public returns(bool) {
+    function transferEther(address payable recipient, uint amount) public returns(bool) {
+        require(owner == msg.sender, "Transfer failed, you are not the owner.");
+
         if (amount <= getBalance()){
             recipient.transfer(amount);
             return true;
